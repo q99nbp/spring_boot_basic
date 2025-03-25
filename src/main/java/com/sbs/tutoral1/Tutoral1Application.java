dbe8ba8161085bf57cf6facc11a84cca4c9c8cc6
lee <q99nbp@gmail.com>
2025-03-25 23:25:43 +0900

    first commit

---
+++ src/main/java/com/sbs/tutoral1/Tutoral1Application.java
@@ -0,0 +1,13 @@
+package com.sbs.tutoral1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tutoral1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tutoral1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sbs/tutoral1/Tutoral1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbs.tutoral1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tutoral1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
