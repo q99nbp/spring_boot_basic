efc3d0cf6ceda34b7b27816ee0c867591c9df7b4
lee <q99nbp@gmail.com>
2025-03-26 00:03:28 +0900

    plus

--- src/main/java/com/sbs/tutoral1/controller_home.java
+++ src/main/java/com/sbs/tutoral1/controller_home.java
@@ -3,6 +3,7 @@ package com.sbs.tutoral1;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -12,4 +13,14 @@ public class controller_home {
   public String showHome(){
     return "어서오세요.";
   }
+
+  @GetMapping("home/plus")
+  @ResponseBody
+  public int showPlus(int a, @RequestParam(defaultValue = "0") int b){
+    int num = 0;
+    num = a - b;
+
+    return num;
+  }
+
 }
